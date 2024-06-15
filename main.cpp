76fec0cd52eafcf2b1efea07aea1457b561bbb01
YJacob620 <155396236+YJacob620@users.noreply.github.com>
2024-06-15 22:58:14 +0300

    v1.1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <random>
 
 #define TREE_SIZE 20
-#define K 10
+#define K 30
 
 using std::cout;
 using std::endl;
@@ -26,7 +26,90 @@ int randomInt(int min, int max) {
     return distr(gen);
 }
 
+
+/* Constant int Tree */
 int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    Tree<int> t;
+    vector<Node<int>> v;
+    // Tree creation
+    {
+        for (int i = 0; i < 15; ++i) {
+            v.push_back(Node<int>(i));
+        }
+        t.add_root(v[0]);
+        
+        for (int i = 1; i < 7; ++i) {
+            t.add_sub_node(v[i - 1], v[i]);
+        }
+        t.add_sub_node(v[0], v[9]);
+        t.add_sub_node(v[1], v[8]);
+        t.add_sub_node(v[4], v[7]);
+        for (int i = 10; i < 13; ++i) {
+            t.add_sub_node(v[i - 1], v[i]);
+        }
+        t.add_sub_node(v[11], v[13]);
+        t.add_sub_node(v[10], v[14]);
+        v[4].left = false;
+        v[6].left = false;
+        v[10].left = false;
+    }
+    
+    // Print tree with different Treeterators
+    {
+        cout << "pre order" << endl;
+        auto it = t.begin_pre_order();
+        auto end = t.end_pre_order();
+        for (; it != end; ++it) {
+            cout << it << " ";
+        }
+        
+        cout << endl << "post order" << endl;
+        it = t.begin_post_order();
+        end = t.end_post_order();
+        for (; it != end; ++it) {
+            cout << it << " ";
+        }
+        
+        cout << endl << "in order" << endl;
+        it = t.begin_in_order();
+        end = t.end_in_order();
+        for (; it != end; ++it) {
+            cout << it << " ";
+        }
+        
+        cout << endl << "bfs" << endl;
+        it = t.begin_bfs_scan();
+        end = t.end_bfs_scan();
+        for (; it != end; ++it) {
+            cout << it << " ";
+        }
+        
+        cout << endl << "dfs" << endl;
+        it = t.begin_dfs_scan();
+        end = t.end_dfs_scan();
+        for (; it != end; ++it) {
+            cout << it << " ";
+        }
+        
+        cout << endl << "min heap" << endl;
+        it = t.myHeap(t.begin_pre_order(), t.begin_in_order());
+        while (*it != nullptr) {
+            cout << it << " ";
+            ++it;
+        }
+        cout << endl;
+    }
+    
+    // Display tree in GUI
+    ~t;
+    
+    return app.exec();
+}
+
+/* Random Complex Tree */
+int main2(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     Tree<Complex> t = Tree<Complex>(K);
@@ -99,32 +182,24 @@ int main(int argc, char *argv[]) {
     return app.exec();
 }
 
-int main2(int argc, char *argv[]) {
+/* Random long Tree */
+int main3(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    Tree<int> t;
-    vector<Node<int>> v;
+    Tree<long> t = Tree<long>(K);
+    vector<Node<long>> v;
+
     // Tree creation
     {
-        for (int i = 0; i < 15; ++i) {
-            v.push_back(Node<int>(i));
+        for (int i = 0; i < TREE_SIZE; ++i) {
+            v.push_back(Node<long>(randomInt(-100,100)));
         }
         t.add_root(v[0]);
 
-        for (int i = 1; i < 7; ++i) {
-            t.add_sub_node(v[i - 1], v[i]);
+        for (unsigned i = 0; i < v.size()-1; ++i) {
+            unsigned j = randomInt(0,i);
+            t.add_sub_node(v[j], v[i+1]);
         }
-        t.add_sub_node(v[0], v[9]);
-        t.add_sub_node(v[1], v[8]);
-        t.add_sub_node(v[4], v[7]);
-        for (int i = 10; i < 13; ++i) {
-            t.add_sub_node(v[i - 1], v[i]);
-        }
-        t.add_sub_node(v[11], v[13]);
-        t.add_sub_node(v[10], v[14]);
-        v[4].left = false;
-        v[6].left = false;
-        v[10].left = false;
     }
 
     // Print tree with different Treeterators
@@ -164,8 +239,9 @@ int main2(int argc, char *argv[]) {
             cout << it << " ";
         }
 
+
         cout << endl << "min heap" << endl;
-        it = t.myHeap(t.begin_pre_order(), t.begin_in_order());
+        it = t.myHeap();
         while (*it != nullptr) {
             cout << it << " ";
             ++it;
